84766bb09aa8db93032695a79c99b1774403730e
lakhvir211 <60156310+lakhvir211@users.noreply.github.com>
2020-04-28 12:11:36 -0400

    Update connection.php

--- bank/core/connection.php
+++ bank/core/connection.php
@@ -7,7 +7,7 @@ date_default_timezone_set('US/Eastern');
 
 
 $dsn = "mysql:host=sql1.njit.edu; dbname=lk268';charset=utf8mb4";
-$user = 'lk268';
+$user = 'lk268@webhost01.ucs.njit.edu';
 $pass = 'Lovefamily2$';
 try {
     $pdo = new PDO($dsn, $user, $pass);
