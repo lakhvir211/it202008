6c3ec17c951e027c4e344b6a1b9c1c89eb924420
lakhvir211 <60156310+lakhvir211@users.noreply.github.com>
2020-04-29 13:27:11 -0400

    Create config.php

---
+++ bank/core/config.php
@@ -0,0 +1,6 @@
+<?php
+$dbuser = "lk268";//your ucid
+$dbpass = "Lovefamily2$";//your phpMyAdmin password
+$dbhost = "sql1.njit.edu";//whichever server you login to from web.njit.edu/mysql/phpMyAdmin
+$dbdatabase = "lk268";//your ucid
+?>
